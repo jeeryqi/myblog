86e94f15e06d28bb1a2ce17830c537f81c5148c7
jeery <jeery_qi@outlook.com>
2018-08-10 15:25:38 +0800

    myblog

--- blog/models.py
+++ blog/models.py
@@ -8,17 +8,20 @@ from django.contrib.auth.models import User
 
 class BlogType(models.Model):
     type_name = models.CharField('博客类型', max_length=20)
-    ordering = 'pk'
 
     def __str__(self):
         return self.type_name
+
+    class Meta:
+        ordering = ('pk')
 
 
 class Blog(models.Model):
     blog_type = models.ForeignKey(to=BlogType, on_delete=models.CASCADE, related_name='blogs', default=1)
     title = models.CharField('标题', max_length=50)
     content = models.TextField('内容')
-    author = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blogs', verbose_name='作者',default=1)
+    author = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blogs',
+                               verbose_name='作者', default=1)
     create_time = models.DateTimeField('发布时间', auto_now_add=True)
     last_update_time = models.DateTimeField('修改时间', auto_now=True)
 
